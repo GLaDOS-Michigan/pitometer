1bdf0c7526322b19ec07f7cb345e58e481cfca97
Tony Zhang <nz97@cornell.edu>
2020-04-10 13:20:50 -0400

    rename folder

--- go-compiler/build.sh
+++ go-compiler/build.sh
@@ -1,4 +1,4 @@
-LOCK_DIR='/Users/zhang/Dropbox/UMich2020sp/pitometer/go-compiler/Main.i-go'
+LOCK_DIR='/Users/zhang/Dropbox/UMich2020sp/pitometer/go-compiler/toylock-go'
 export GOPATH=$LOCK_DIR
 
 go build "$LOCK_DIR/src/Main.go"
