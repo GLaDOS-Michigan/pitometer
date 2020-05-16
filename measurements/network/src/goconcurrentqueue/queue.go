bf0f25d14ef151776076f1f5892cddb8109202ad
Tony Zhang <nz97@cornell.edu>
2020-05-16 14:04:30 -0400

    fix bug where program crashes due to too many pending DequeueOrWaitForNextElement() calls when a timeout is triggered.

--- measurements/network/src/goconcurrentqueue/queue.go
+++ measurements/network/src/goconcurrentqueue/queue.go
@@ -9,6 +9,11 @@ type Queue interface {
 	// DequeueOrWaitForNextElement dequeues an element (if exist) or waits until the next element gets enqueued and returns it.
 	// Multiple calls to DequeueOrWaitForNextElement() would enqueue multiple "listeners" for future enqueued elements.
 	DequeueOrWaitForNextElement() (interface{}, error)
+	// DequeueOrWaitForNextElementMax1 calls DequeueOrWaitForNextElement, while ensuring that
+	// there are no more than 1 calls to DequeueOrWaitForNextElement() waiting
+	DequeueOrWaitForNextElementMax1() (interface{}, error)
+	// DequeueOrWaitForNextElementCancel deletes the wait channel from the previous DequeOrWaitForNextElement request
+	DequeueOrWaitForNextElementCancel() (bool, error)
 	// Get number of enqueued elements
 	GetLen() int
 	// Get queue's capacity
