07d632dfcdc66aab3141ada552c5712bf71940a4
Tony Zhang <nz97@cornell.edu>
2020-05-21 12:35:34 -0400

    update to accomodate repeated trials

--- measurements/experiments/analyze_network.py
+++ measurements/experiments/analyze_network.py
@@ -9,40 +9,46 @@ from matplotlib.offsetbox import AnchoredText
 from matplotlib.backends.backend_pdf import PdfPages
 import seaborn as sns
 
-NODES = [1, 2, 3, 4, 5, 6]
+NODES = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ,20]
+PAYLOADS = [4, 16, 32, 128, 512]
 NODES.sort()
+PAYLOADS.sort()
 THROWAWAY = 10 # Number of starting readings to throw away
 
 def main(exp_dir):
     exp_dir = os.path.abspath(exp_dir)
     print("\nAnalyzing data for experiment %s" %exp_dir)
-    for root, dirs, files in os.walk(exp_dir):
-        files = [f for f in files if not f[0] == '.']  # ignore hidden files
-        if dirs == []:
-            # This is a leaf directory containing trial csv files
-            print("\tAnalyzing trial %s" %root)
-            
-            # Grab the payload of this sub-experiment
-            payload = int(root.split('payload')[-1])
+    for payload in PAYLOADS:
+        print("\tAnalyzing payload %d" %payload)
+        # {2D map} total_data[i][j] is the timings for node i to node j for this payload
+        total_payload_data = dict()  
+        for root, dirs, files in os.walk("%s/payload%d" %(exp_dir, payload)):
+            files = [f for f in files if not f[0] == '.']  # ignore hidden files
+            if dirs == []:
+                # This is a leaf directory containing trial csv files
+                print("\t\tAnalyzing trial %s" %root)
+                
+                for i in NODES:
+                    grep_str = "node%d" %i
+                    nodei_csvs= [f for f in files if grep_str in f.split('-')[0] and ".csv" in f]
+                    nodei_csvs.sort()
+                    if i not in total_payload_data:
+                        total_payload_data[i] = dict()
 
-            # {2D map} total_data[i][j] is the timings for node i to node j
-            total_data = dict()
-
-            for i in NODES:
-                grep_str = "node%d" %i
-                nodei_csvs= [f for f in files if grep_str in f.split('-')[0] and ".csv" in f]
-                nodei_csvs.sort()
-                total_data[i] = dict()
-
-                for j in NODES:
-                    # File for log of nodei->nodej
-                    # print(NODES[j], j)
-                    i_j_csv = nodei_csvs[NODES.index(j)]
-                    grep_str = "node%d" %j
-                    assert grep_str in i_j_csv.split('-')[1]  # sanity check to make sure we have the right target file
-                    total_data[i][j] = analyze_csv("%s/%s" %(root, i_j_csv))
-            print("\tDrawing trial %s" %root)
-            plot_figures("rtt_payload%d" %payload, root, total_data)
+                    for j in NODES:
+                        # File for log of nodei->nodej
+                        if j not in total_payload_data[i]:
+                            total_payload_data[i][j] = []
+                        # find the csv for nodei->nodej
+                        grep_str = "node%d." %j
+                        i_j_csv = None
+                        for candidate in nodei_csvs:
+                            if grep_str in candidate.split('-')[1]:
+                                i_j_csv = candidate
+                        if i_j_csv is not None:
+                            total_payload_data[i][j].extend(analyze_csv("%s/%s" %(root, i_j_csv)))
+        print("\tDrawing payload %d" %payload)
+        plot_figures("rtt_payload%d" %payload, exp_dir, total_payload_data)
     print("Done")
 
 
@@ -135,17 +141,6 @@ def plot_individuals(pp, name, root, total_data):
             # this_ax.set_ylim(0, 1)
             col += 1
         row += 1
-    # Display some global figures
-    global_data = []
-    for i in total_data.keys():
-        for j in total_data[i].keys():
-            global_data.extend(total_data[i][j])
-
-    global_stats =  "Global statistics:\n%s" %generate_statistics(global_data)
-    plt.figtext(0.8, 0.91, global_stats,
-            fontsize=12,
-            bbox=dict(boxstyle="round", facecolor='#D8D8D8',
-            ec="0.5", pad=0.5, alpha=1), fontweight='bold')
 
     # Draw plot
     plt.subplots_adjust(hspace=0.2, wspace=0.3)
@@ -164,7 +159,7 @@ def generate_statistics(input):
     if len(input) == 0:
         return ""
     res = []
-    res.append("n = %d" %len(input))
+    res.append(f"n = {'{:,}'.format(len(input))}")
     res.append("μ = %.3f" %statistics.mean(input))
     res.append("σ = %.4f" %statistics.stdev(input))
     res.append("99.9%% = %.3f" %np.percentile(input, 99.9))
