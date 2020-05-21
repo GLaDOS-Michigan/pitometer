b156982a255d384d369265c21a964b877e0932ca
Tony Zhang <nz97@cornell.edu>
2020-05-20 21:34:08 -0400

    update heartbeat

--- measurements/rsl/rsl-go/src/236_LiveRSL____ParametersState__i_Compile_/236_LiveRSL____ParametersState__i_Compile_.go
+++ measurements/rsl/rsl-go/src/236_LiveRSL____ParametersState__i_Compile_/236_LiveRSL____ParametersState__i_Compile_.go
@@ -304,7 +304,7 @@ func (_this *CompanionStruct_Default___) StaticParams() ParametersState {
 		// make heartbeats infrequent such that they don't interfere with our message queues
 		Max__log__length:                uint64(2_000),
 		Baseline__view__timeout__period: uint64(1000),
-		Heartbeat__period:               uint64(20_000),
+		Heartbeat__period:               uint64(30_000),
 		Max__integer__val:               (uint64(9223372036854775808)) - (func() uint64 { return (uint64(1)) })(),
 		Max__batch__size:                uint64(32),
 		Max__batch__delay:               uint64(10)}}
