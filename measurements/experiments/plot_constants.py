5d47d5eb3c0d97e43a52ffdd51e153a64972a610
Tony Zhang <nz97@cornell.edu>
2020-05-24 17:20:01 -0400

    update drawing parameters

--- measurements/experiments/plot_constants.py
+++ measurements/experiments/plot_constants.py
@@ -2,14 +2,21 @@ import matplotlib.pyplot as plt
 
 # Global plotting params
 # Fonts
-plt.rc('font', family='serif') 
-plt.rc('font', size=12)              # controls default text sizes
-plt.rc('axes', titlesize=13)        # fontsize of the axes title
-plt.rc('axes', labelsize=12)        # fontsize of the x and y labels
-plt.rc('xtick', labelsize=12)       # fontsize of the tick labels
-plt.rc('ytick', labelsize=12)       # fontsize of the tick labels
-plt.rc('legend', fontsize=12)       # legend fontsize
-plt.rc('figure', titlesize=12)      # fontsize of the figure title
+base_size = 7
+plt.rc('font', family='sans-serif') 
+plt.rc('font', size=base_size)              # controls default text sizes
+plt.rc('axes', titlesize=base_size)        # fontsize of the axes title
+plt.rc('axes', labelsize=base_size)        # fontsize of the x and y labels
+plt.rc('xtick', labelsize=base_size)       # fontsize of the tick labels
+plt.rc('ytick', labelsize=base_size)       # fontsize of the tick labels
+plt.rc('legend', fontsize=base_size)       # legend fontsize
+# plt.rc('figure', titlesize=12)      # fontsize of the figure title
 
-# Lines
-plt.rc('lines', linewidth=1)
+# Graphics
+plt.rc('lines', linewidth=0.5)
+plt.rc('lines', markersize=1.5)
+plt.rc('legend', frameon=False)        # No frame around legend
+
+# Figure size
+fig_width = 3
+fig_height = 2.5
