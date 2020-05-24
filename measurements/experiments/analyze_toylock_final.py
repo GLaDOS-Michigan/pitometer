cdec7677c4e757ac68270d1f8d8a7d75a1f5853b
Tony Zhang <nz97@cornell.edu>
2020-05-24 16:42:47 -0400

    plot microbenchmark 1

--- measurements/experiments/analyze_toylock_final.py
+++ measurements/experiments/analyze_toylock_final.py
@@ -43,31 +43,70 @@ def main(exp_dir):
     print("\nPlotting graphs for experiment %s" %exp_dir)
 
     # Plot Rounds
-    plot_micro_2_size_fidelity("Micro-benchmark 2", exp_dir, total_round_data)
+    plot_micro_1_distr_fidelity("Micro-benchmark1", exp_dir, total_round_data, total_grant_data, total_accept_data)
+    plot_micro_2_size_fidelity("Micro-benchmark2", exp_dir, total_round_data)
     print("Done")
 
 
-def plot_micro_1_distr_fidelity(name, root, total_round_data):
+def plot_micro_1_distr_fidelity(name, root, total_round_data, total_grant_data, total_accept_data):
     """ Plot a figure where each subfigure is from an element in total_data
     Arguments:
         name -- name of this figure
         root -- directory to save this figure
         total_data -- dict of (size -> delay -> node -> [ durs ])
     """
-    with PdfPages("%s/%s.pdf" %(root, name)) as pp:
-        for delay in DELAYS:
+    print("Plotting graphs for Micro-benchmark 1")
+    for delay in DELAYS:
+        with PdfPages("%s/%s_%d.pdf" %(root, name, delay)) as pp:
             x_vals_ring_size = sorted(list(total_round_data.keys()))
-            y_vals_ninety_nine_point_nine_percentiles = []
-            y_vals_fifty_percentiles = []
             for ring_size in x_vals_ring_size:
-                leader_node = sorted(list(total_round_data[ring_size][delay].keys()))[0]
-                y_vals_ninety_nine_point_nine_percentiles.append(np.percentile(total_round_data[ring_size][delay][leader_node], 99.9))
-                y_vals_fifty_percentiles.append(np.percentile(total_round_data[ring_size][delay][leader_node], 50))
-            fig, this_ax = plt.subplots(1, 1, figsize=(8.5, 5), sharex=False)
-            plot_micro_2_size_fidelity_ax(this_ax, "delay %.1f" %(delay/1000.0), x_vals_ring_size, y_vals_fifty_percentiles, y_vals_ninety_nine_point_nine_percentiles)
-            pp.savefig(fig)
-            plt.close(fig)
+                participants = sorted(list(total_round_data[ring_size][delay].keys()))
+                leader_node = participants[0]
+                actual_round_latencies = total_round_data[ring_size][delay][leader_node]
+                actual_grant_latencies, actual_accept_latencies = compute_actual_grant_accept(total_grant_data, total_accept_data, delay, ring_size)
+                fig, this_ax = plt.subplots(1, 1, figsize=(8.5, 5), sharex=False)
+                plot_micro_1_distr_fidelity_ax(this_ax, "ring size %.d" %(ring_size), actual_round_latencies, actual_grant_latencies, actual_accept_latencies)
+                pp.savefig(fig)
+                plt.close(fig)
 
+def compute_actual_grant_accept(total_grant_data, total_accept_data, delay, ring_size):
+    """Compute the aggregate grant and accept latencies for this delay and ring_size
+    Arguments:
+        total_grant_data {dict} -- dict of (size -> delay -> node -> [ durs ])
+        total_accept_data {dict} -- dict of (size -> delay -> node -> [ durs ])
+        delay {int} -- [description]
+        ring_size {int} -- [description]
+    """
+    aggregate_grant_latencies = []
+    aggregate_accept_latencies = []
+    for node in total_grant_data[ring_size][delay].keys():
+        aggregate_grant_latencies.extend(total_grant_data[ring_size][delay][node])
+    for node in total_accept_data[ring_size][delay].keys():
+        aggregate_accept_latencies.extend(total_accept_data[ring_size][delay][node])
+    return aggregate_grant_latencies, aggregate_accept_latencies
+
+
+def plot_micro_1_distr_fidelity_ax(
+    this_ax, 
+    name, 
+    actual_round_latencies, 
+    actual_grant_latencies, 
+    actual_accept_latencies
+):
+    show_hist = False
+    kwargs = {'cumulative': True}
+    this_ax.set_xlabel('cumulative probability', fontsize=10)
+    this_ax.set_ylabel('latency (ms)', fontsize=10)
+    this_ax.set_title(name)
+    sns.distplot(actual_round_latencies, hist=show_hist, hist_kws=kwargs, kde_kws=kwargs, vertical=True, label='round')
+    sns.distplot(actual_grant_latencies, hist=show_hist, hist_kws=kwargs, kde_kws=kwargs, vertical=True, label='grant')
+    sns.distplot(actual_accept_latencies, hist=show_hist, hist_kws=kwargs, kde_kws=kwargs, vertical=True, label='accept')
+    this_ax.set_xlim(0, 1)
+    # this_ax.set_ylim(0, np.percentile(actual_round_latencies, 99))
+    this_ax.set_yscale("log")
+    this_ax.grid()
+    this_ax.xaxis.set_ticks(np.arange(0, 1, 0.1))
+    this_ax.legend()
 
 
 def plot_micro_2_size_fidelity(name, root, total_round_data):
@@ -77,6 +116,7 @@ def plot_micro_2_size_fidelity(name, root, total_round_data):
         root -- directory to save this figure
         total_data -- dict of (size -> delay -> node -> [ durs ])
     """
+    print("Plotting graphs for Micro-benchmark 1")
     with PdfPages("%s/%s.pdf" %(root, name)) as pp:
         for delay in DELAYS:
             x_vals_ring_size = sorted(list(total_round_data.keys()))
