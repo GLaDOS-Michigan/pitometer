accffabbcf110d6fa2b5f2b2a9c82e8975d69f4c
Tony Zhang <nz97@cornell.edu>
2020-05-24 16:55:56 -0400

    use a python file to store common plotting constants

---
+++ measurements/experiments/plot_constants.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+# Global plotting params
+# Fonts
+plt.rc('font', family='serif') 
+plt.rc('font', size=12)              # controls default text sizes
+plt.rc('axes', titlesize=13)        # fontsize of the axes title
+plt.rc('axes', labelsize=12)        # fontsize of the x and y labels
+plt.rc('xtick', labelsize=12)       # fontsize of the tick labels
+plt.rc('ytick', labelsize=12)       # fontsize of the tick labels
+plt.rc('legend', fontsize=12)       # legend fontsize
+plt.rc('figure', titlesize=12)      # fontsize of the figure title
+
+# Lines
+plt.rc('lines', linewidth=1)
