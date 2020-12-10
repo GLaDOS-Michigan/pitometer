6b3d31b5db658be7d538b158e608a80f3bec6bec
Tony Zhang <nz97@cornell.edu>
2020-12-09 19:11:47 -0500

    sleep between requests

--- ironfleet/src/IronfleetClient/IronfleetClient/MultiPaxos.cs
+++ ironfleet/src/IronfleetClient/IronfleetClient/MultiPaxos.cs
@@ -144,8 +144,9 @@
                     var dest = ClientBase.endpoints[serverIdx];
                     Trace("Client " + id.ToString() + ": Sending a request with a sequence number " + msg.GetSeqNum() + " to " + dest);
 
-                    this.Send(msg, dest);
+                    Thread.Sleep(10);
                     var start_time = HiResTimer.Ticks;
+                    this.Send(msg, dest);
                     //foreach (var remote in ClientBase.endpoints)
                     //{
                     //    this.Send(msg, remote);
