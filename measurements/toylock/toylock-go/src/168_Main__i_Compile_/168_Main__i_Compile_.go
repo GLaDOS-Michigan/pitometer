4d569843660177c662c012d2823f724133ff7b80
Tony Zhang <nz97@cornell.edu>
2021-03-31 12:44:51 -0400

    update clock implementation to be more performant

--- measurements/toylock/toylock-go/src/168_Main__i_Compile_/168_Main__i_Compile_.go
+++ measurements/toylock/toylock-go/src/168_Main__i_Compile_/168_Main__i_Compile_.go
@@ -188,8 +188,8 @@ func (_this *CompanionStruct_Default___) Default_Main_(numRounds int, delay int)
 	_1764_id = _out110
 	{
 	}
-	var nodeGrantLog = clock.NewStopwatch(uint(numRounds*2), "NodeGrant Stopwatch")
-	var nodeAcceptLog = clock.NewStopwatch(uint(numRounds*2), "NodeAccept Stopwatch")
+	var nodeGrantLog = clock.NewStopwatch("NodeGrant Stopwatch")
+	var nodeAcceptLog = clock.NewStopwatch("NodeAccept Stopwatch")
 	var nodeGrantCounter = clock.NewCounter("NodeGrant counter")
 	for _1761_ok {
 		nodeGrantLog.LogStartEvent("NodeNextGrant")
@@ -211,11 +211,9 @@ func (_this *CompanionStruct_Default___) Default_Main_(numRounds int, delay int)
 		}
 		if nodeGrantCounter.GetCount() == numRounds {
 			// nodeAcceptLog.PopStartEvent()
+			fmt.Printf("Complete %d rounds\n", numRounds)
 			nodeGrantLog.PopStartEvent()
-			// Dump the event log
-			nodeGrantLog.PrintLog()
-			nodeAcceptLog.PrintLog()
-			time.Sleep(1 * time.Second)
+			time.Sleep(500 * time.Millisecond)
 			os.Exit(0)
 		}
 		switch status {
@@ -223,11 +221,9 @@ func (_this *CompanionStruct_Default___) Default_Main_(numRounds int, delay int)
 			nodeAcceptLog.LogEndEvent("NodeNextAccept")
 			nodeGrantLog.PopStartEvent()
 		case "accept fail":
-			// nodeAcceptLog.PopStartEvent()
 			nodeGrantLog.PopStartEvent()
 		case "grant ok":
 			nodeGrantLog.LogEndEvent("NodeNextGrant")
-			// nodeAcceptLog.PopStartEvent()
 		default:
 			fmt.Printf("Error: unexpected main loop result")
 		}
