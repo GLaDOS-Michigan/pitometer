937f8e3419f3466750305694bcbbf8b65811bfa9
Tony Zhang <nz97@cornell.edu>
2021-04-20 13:41:36 -0400

    init

---
+++ measurements/network-aws/src/agents/client.go
@@ -0,0 +1,98 @@
+package agents
+
+import (
+	"clock"
+	"fmt"
+	native "native"
+	"net"
+	"os"
+	"time"
+)
+
+// ClientTimeOut duration
+var ClientTimeOut time.Duration
+
+// Client is an agent that sends UDP packets and times their round-trip time (RTT)
+type Client struct {
+	LocalAddr    *net.UDPAddr
+	Target       *net.UDPAddr     // remote address to send packet
+	Interval     uint64           // milliseconds to sleep in between pings
+	PacketSize   uint64           // size of UDP payload to send
+	PingLog      *clock.Stopwatch // record of ping events
+	TimeoutCount *clock.Counter   // count timeout events
+	active       bool
+}
+
+// StartClientLoop is the main event loop of the Client. It receives UDP packets and
+// bounces them back to the sender
+func (c *Client) StartClientLoop() {
+	var ok, udpClient = native.NewUDPClient(native.UDPAddrToIPEndPoint(c.LocalAddr), 1, 1)
+
+	if !ok {
+		fmt.Printf("Error: failed to initialize client\n")
+		os.Exit(1)
+	}
+
+	// Craft packet and initialize dest
+	var payload = make([]byte, c.PacketSize, c.PacketSize)
+	var dest = native.UDPAddrToIPEndPoint(c.Target)
+	var pack = &native.Packet{EndPoint: dest, Buffer: payload[0:]}
+	c.active = true
+
+	fmt.Printf("Starting new client at %v targeting %v\n", c.LocalAddr, c.Target)
+	var sendNote = fmt.Sprintf("Send to target,%v", c.Target)
+	var receiveNote = fmt.Sprintf("Receive from target,%v", c.Target)
+	// Main event loop
+	for c.active {
+
+		// Send packet
+		native.Debug(fmt.Sprintf("Client %v sending %v", c.LocalAddr, pack))
+		c.PingLog.LogStartEvent(sendNote)
+		udpClient.Send(pack)
+
+		// Receive packet
+		var remote *native.IPEndPoint = nil
+		var receivedPacket *native.Packet = nil
+		var timedOutChan = make(chan bool, 2)
+		go func(c *Client, timedOutChan chan bool) {
+			_, _, remote, receivedPacket = udpClient.Receive()
+			native.Debug(fmt.Sprintf("Client %v received response from %v, %v", c.LocalAddr, remote.GetUDPAddr(), receivedPacket))
+			c.PingLog.LogEndEvent(receiveNote)
+			timedOutChan <- false
+		}(c, timedOutChan)
+		go func(timedOutChan chan bool) {
+			time.Sleep(ClientTimeOut)
+			timedOutChan <- true
+		}(timedOutChan)
+
+		var timedOut = <-timedOutChan
+
+		if timedOut {
+			native.Debug("Timed out!")
+			ok, err := udpClient.ReceiveQueue.DequeueOrWaitForNextElementCancel()
+			if !ok {
+				fmt.Printf("Error: DequeueOrWaitForNextElementCancel failed. %v\n", err)
+				os.Exit(1)
+			}
+			// This event is a timeout
+			c.PingLog.PopStartEvent()
+			c.TimeoutCount.Increment()
+			continue
+		}
+		if len(receivedPacket.Buffer) != int(c.PacketSize) {
+			fmt.Printf("Error: got packet length %v, expected %v\n",
+				len(receivedPacket.Buffer),
+				c.PacketSize)
+			os.Exit(1)
+		}
+
+		// Sleep
+		time.Sleep(time.Duration(c.Interval) * time.Millisecond)
+	}
+	fmt.Printf("Client at %v targeting %v deactivated\n", c.LocalAddr, c.Target)
+}
+
+// StopClientLoop stops the main event loop of the Client.
+func (c *Client) StopClientLoop() {
+	c.active = false
+}
---
+++ measurements/network-aws/src/clock/clock.go
@@ -0,0 +1,122 @@
+package clock
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+/*****************************************************************************************
+************************************* TimePoint ******************************************
+*****************************************************************************************/
+
+// TimeInterval contains the information of a clock event
+type TimeInterval struct {
+	id           int
+	functionName string
+	startTime    time.Duration //start time of this interval
+	endTime      time.Duration //end time of this interval
+}
+
+// newTimeInterval creates a new TimeInterval object with void start and end times
+func newTimeInterval(id int, name string) *TimeInterval {
+	return &TimeInterval{
+		id:           id,
+		functionName: name,
+		startTime:    0,
+		endTime:      0}
+}
+
+func (ti *TimeInterval) logStartTime(st time.Duration) {
+	ti.startTime = st
+}
+
+func (ti *TimeInterval) logEndTime(et time.Duration) {
+	ti.endTime = et
+}
+
+// PrintTimeInterval prints the time interval
+func (ti *TimeInterval) PrintTimeInterval() {
+	fmt.Printf("%d,%s,%v,%v\n", ti.id, ti.functionName, ti.startTime.Nanoseconds(), ti.endTime.Nanoseconds())
+}
+
+/*****************************************************************************************
+************************************** Stopwatch *****************************************
+*****************************************************************************************/
+
+// Stopwatch represents a sequence of TimePoints
+type Stopwatch struct {
+	name         string
+	initTime     time.Time
+	currInterval *TimeInterval
+	nextIndex    int
+}
+
+// NewStopwatch generates a new log pre-initialized to length n
+func NewStopwatch(name string) *Stopwatch {
+	var s = &Stopwatch{
+		name:         name,
+		initTime:     time.Now(),
+		currInterval: nil,
+		nextIndex:    0}
+	fmt.Printf("stopwatch init,%s,%v\n", s.name, s.initTime)
+	return s
+}
+
+// LogStartEvent adds a temp start event to currInterval in the stopwatch
+func (el *Stopwatch) LogStartEvent(name string) {
+	if el.currInterval != nil {
+		fmt.Printf("Error: Pending interval already present\n")
+		os.Exit(1)
+	}
+	var ti = newTimeInterval(el.nextIndex, name)
+	ti.logStartTime(time.Since(el.initTime))
+	el.currInterval = ti
+}
+
+// LogEndEvent adds a new end event to the log
+func (el *Stopwatch) LogEndEvent(name string) {
+	if el.currInterval == nil {
+		fmt.Printf("Error: No start time recorded\n")
+		os.Exit(1)
+	}
+	var ti = el.currInterval
+	ti.logEndTime(time.Since(el.initTime))
+	ti.PrintTimeInterval()
+	el.nextIndex++
+	el.currInterval = nil
+}
+
+// PopStartEvent deletes the last event from the log, which must be a start event
+func (el *Stopwatch) PopStartEvent() {
+	if el.currInterval == nil {
+		fmt.Printf("Error: No start time recorded\n")
+		os.Exit(1)
+	}
+	el.currInterval = nil
+}
+
+/*****************************************************************************************
+************************************** Counter *******************************************
+*****************************************************************************************/
+
+// Counter is used to count events in code
+type Counter struct {
+	count int
+	name  string
+}
+
+// NewCounter is the constructor for a Counter
+func NewCounter(name string) *Counter {
+	return &Counter{0, name}
+}
+
+// Increment increments the counter by 1
+func (c *Counter) Increment() {
+	c.count++
+}
+
+// GetCount returns the count of the counter
+func (c *Counter) GetCount() int {
+	return c.count
+}
