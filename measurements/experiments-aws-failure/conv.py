61e022302cc46953cdb161adf2be75a7c40d45c6
Tony Zhang <nz97@cornell.edu>
2021-12-14 01:22:21 -0500

    failure exp

---
+++ measurements/experiments-aws-failure/conv.py
@@ -0,0 +1,56 @@
+import numpy as np
+from scipy import signal
+
+def get_mean(pdf, bins):
+    sum = 0
+    for i in range(len(pdf)):
+        sum += pdf[i] * float(bins[i])
+    return sum
+
+def get_percentile(cdf, bins, percentile):
+    assert percentile >= 0 and percentile <= 100
+    for i in range(len(cdf)):
+        if cdf[i] > percentile/100.0:
+            return bins[i]
+    return bins[-1]
+
+def raw_data_to_cdf(data):
+    sorted_data = sorted(data)
+    cdf, bins = [], []
+    n = len(data) * 1.0
+    for i in range(len(data)):
+        cdf.append(i/n)
+        bins.append(sorted_data[i])
+    return [0] + cdf, [bins[0]] + bins
+
+def raw_data_to_pdf(data, binsize):
+    bincount = int((max(data) - min(data))/binsize)
+    bins = np.linspace(min(data), max(data), bincount)
+    pdf, bins = np.histogram(data, bins=bins)
+    return pdf, bins
+
+def pdf_to_cdf(pdf):
+    cdf = np.cumsum(pdf/pdf.sum()).tolist()
+    return cdf
+
+def cdf_to_pdf(cdf):
+    dx = 1.0 / len(cdf)
+    pdf = [0]
+    for i in range(len(cdf)-1):
+        pdf.append((cdf[i+1]-cdf[i])/dx)
+    
+    return np.array(pdf), dx
+
+
+def add_histograms(pdf1, pdf2, start1, start2, binsize1, binsize2):
+    """
+    pdf{j} should be a seq of numbers representing the histogram for the jth
+    distribution. Assumes that the numbers in the sequence pdf{j} sum to 1
+    start{j} is the starting of pmf1
+    """
+    conv_pdf = signal.fftconvolve(pdf1,pdf2,'full')
+    conv_pdf = conv_pdf/float(conv_pdf.sum()) # This should be unnecessary, but
+                                              # keeping it just in case pdf1 and pdf2 don't have a sum of 1
+    binsize_out = binsize1 # + binsize2
+    start_out = start1 + start2
+    return conv_pdf, start_out, binsize_out
